11d555b53ee98b2988a952b1198658be6af1f86d
vldrshv <vldrshv@gmail.com>
2021-01-30 13:59:01 +0300

    Initial Commit

---
+++ iosui/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  iosui
+//
+//  Created by vlad on 30.01.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
