dfc9f14edfd98a618df1556fb389e607c73aec96
vldrshv <vldrshv@gmail.com>
2021-02-12 22:19:07 +0300

    dz 5
    login and password redesign + error enabled

--- iosui/login/ViewController.swift
+++ iosui/login/ViewController.swift
@@ -12,16 +12,20 @@ class ViewController: UIViewController {
     @IBOutlet weak var loginText: UITextField!
     @IBOutlet weak var passwordText: UITextField!
     @IBOutlet weak var loginButton: UIButton!
+    @IBOutlet weak var errorView: UIView!
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
-        loginButton.layer.cornerRadius = 12
-        loginText.layer.cornerRadius = 12
-        passwordText.layer.cornerRadius = 12
+        addShadow(to: [loginButton])
+        addCornerRadius(to: [loginText, passwordText, loginButton, errorView])
+        errorView.isHidden = true
     }
     
-
+    @IBAction func onEditting(_ sender: Any) {
+        showError(show: false)
+    }
+    
     @IBAction func onLoginClicked(_ sender: Any) {
         AnimationUtil.scale(v: loginButton, scaleTo: 0.95)
         
@@ -30,10 +34,10 @@ class ViewController: UIViewController {
         print(login)
         print(password)
         
-        if login != "v" && password != "v" {
-            return // todo show invalid
-        } else {
+        if login == "v" && password == "v" {
             self.dismiss(animated: false, completion: { self.makeLogin() })
+        } else {
+            showError(show: true)
         }
     }
     
@@ -41,6 +45,36 @@ class ViewController: UIViewController {
         let sb = UIStoryboard(name: "BottomTabs", bundle: nil)
         let vc = sb.instantiateViewController(withIdentifier: "BottomTabsController")
         self.present(vc, animated: true, completion: nil)
+    }
+    
+    private func showError(show: Bool) {
+        if show && self.errorView.isHidden {
+            self.errorView.isHidden = false
+        } else {
+            if !show && !self.errorView.isHidden {
+                self.errorView.isHidden = true
+            }
+        }
+    }
+    
+    private func addShadow(to: [UIView]) {
+        for view in to {
+            view.layer.masksToBounds = false
+            view.layer.shadowColor = UIColor.black.cgColor
+            view.layer.shadowOffset = CGSize(width: 0.0, height: 2.0)
+            view.layer.shadowRadius = 15
+            view.layer.shadowOpacity = 0.5
+            
+            view.layer.shouldRasterize = true
+            view.layer.rasterizationScale = UIScreen.main.scale
+        }
+    }
+    
+    private func addCornerRadius(to: [UIView], cornerRadius: CGFloat = 12) {
+        for view in to {
+            print("corner")
+            view.layer.cornerRadius = cornerRadius
+        }
     }
 }
 
